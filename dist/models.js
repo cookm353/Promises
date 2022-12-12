e47c8ea38a3a99598ea47d73cdb2a51ac0fa741f
Matthew Cook <cookm353@gmail.com>
2022-12-12 17:35:09 -0500

    (mostly) done with step 3

--- dist/models.js
+++ dist/models.js
@@ -111,3 +111,74 @@ class Deck {
         });
     }
 }
+class Pokemon {
+    baseURL = 'https://pokeapi.co/api/v2/pokemon/';
+    speciesURL = 'https://pokeapi.co/api/v2/pokemon-species/';
+    allPokemon = [];
+    constructor() {
+        this.catchEmAll;
+    }
+    catch(id) {
+        /* Make an API call for an individual Pokemon */
+        return new Promise((resolve, reject) => {
+            const resp = axios.get(`${this.baseURL}${id}`);
+            resolve(resp);
+            reject('Rejected!');
+        });
+    }
+    catchEmAll() {
+        /* Populate array with info on every Pokemon */
+        const pokemonPromises = [];
+        for (let id = 1; id <= 905; id++) {
+            pokemonPromises.push(this.catch(id));
+        }
+        Promise.all(pokemonPromises)
+            .then(resps => {
+            resps.forEach(resp => {
+                const info = {
+                    pokemonName: resp.data.name,
+                    pokemonURL: `${this.baseURL}${resp.data.name}`
+                };
+                this.allPokemon.push(info);
+            });
+        })
+            .catch(err => console.log(err));
+    }
+    getRandomIndex() {
+        /* Generate a random index to pick a Pokemon */
+        const min = 0;
+        const max = this.allPokemon.length - 1;
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    pick3() {
+        /* Randomly select 3 Pokemon and display details */
+        const pokemonDetails = [];
+        const pokemon1 = this.allPokemon[this.getRandomIndex()];
+        const pokemon2 = this.allPokemon[this.getRandomIndex()];
+        const pokemon3 = this.allPokemon[this.getRandomIndex()];
+        const pokemonList = [
+            axios.get(`${this.speciesURL}${pokemon1.pokemonName}`),
+            axios.get(`${this.speciesURL}${pokemon2.pokemonName}`),
+            axios.get(`${this.speciesURL}${pokemon3.pokemonName}`)
+        ];
+        axios.get(`${this.speciesURL}${pokemon1.pokemonName}`)
+            .then(resp => {
+            const flavor_text = resp.data.flavor_text_entries[0].flavor_text;
+            console.log(pokemon1.pokemonName);
+            console.log(flavor_text);
+            return axios.get(`${this.speciesURL}${pokemon2.pokemonName}`);
+        })
+            .then(resp => {
+            const flavor_text = resp.data.flavor_text_entries[0].flavor_text;
+            console.log(pokemon2.pokemonName);
+            console.log(flavor_text);
+            return axios.get(`${this.speciesURL}${pokemon3.pokemonName}`);
+        })
+            .then(resp => {
+            console.log(pokemon3.pokemonName);
+            const flavor_text = resp.data.flavor_text_entries[0].flavor_text;
+            console.log(flavor_text);
+        });
+        // return [pokemon1, pokemon2, pokemon3]
+    }
+}
